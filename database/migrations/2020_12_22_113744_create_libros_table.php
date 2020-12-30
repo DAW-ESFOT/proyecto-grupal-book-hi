1db9d7ca4efbc97f25b5a0c1fea10af9ae55b18d
ChristianChalacan <66123229+ChristianChalacan@users.noreply.github.com>
2020-12-29 19:08:32 -0500

    correction table books

--- database/migrations/2020_12_22_113744_create_libros_table.php
+++ database/migrations/2020_12_22_113744_create_libros_table.php
@@ -22,11 +22,11 @@ class CreateLibrosTable extends Migration
             $table->float('price');
             $table->integer('pages');
             $table->text('synopsis');
-            $table->char('book_status');
+            $table->enum('book_status',['new','used']);
             $table->string('cover_page');
             $table->string('back_cover');
-            $table->char('donation');
-            $table->char('available_status');
+            $table->enum('donation',['yes','no']);
+            $table->enum('available_status',['available', 'unavailable']);
             $table->timestamps();
         });
     }
