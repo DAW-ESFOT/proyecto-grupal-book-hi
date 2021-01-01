3f69a24e3cf69bb83b99f3618481ccd45ae476ff
Jaela-C <jaelacarlosama3c@hotmail.com>
2020-12-31 19:08:56 -0500

    #33 Implementation JWT

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -39,26 +37,38 @@ use Illuminate\Support\Facades\Route;
     return 204;
     });*/
 
-    Route::get('books', 'App\\Http\\Controllers\\BookController@index');
-    Route::get('books/{book}', 'App\\Http\\Controllers\\BookController@show');
-    Route::post('books', 'App\\Http\\Controllers\\BookController@store');
-    Route::put('books/{book}', 'App\\Http\\Controllers\\BookController@update');
-    Route::delete('books/{book}', 'App\\Http\\Controllers\\BookController@delete');
+    //Route::get('users', 'App\\Http\\Controllers\\UserController@index');
+    //Route::get('users/{user}', 'App\\Http\\Controllers\\UserController@show');
+    //Route::post('users', 'App\\Http\\Controllers\\UserController@store');
+    //Route::put('users/{user}', 'App\\Http\\Controllers\\UserController@update');
+    //Route::delete('users/{user}', 'App\\Http\\Controllers\\UserController@delete');
 
-    Route::get('chat', 'App\\Http\\Controllers\\ChatController@index');
-    Route::get('chat/{chat}', 'App\\Http\\Controllers\\ChatController@show');
-    Route::post('chat', 'App\\Http\\Controllers\\ChatController@store');
-    Route::put('chat/{chat}', 'App\\Http\\Controllers\\ChatController@update');
-    Route::delete('chat/{chat}', 'App\\Http\\Controllers\\ChatController@delete');
 
-    Route::get('users', 'App\\Http\\Controllers\\UserController@index');
-    Route::get('users/{user}', 'App\\Http\\Controllers\\UserController@show');
-    Route::post('users', 'App\\Http\\Controllers\\UserController@store');
-    Route::put('users/{user}', 'App\\Http\\Controllers\\UserController@update');
-    Route::delete('users/{user}', 'App\\Http\\Controllers\\UserController@delete');
-
+    Route::post('register', 'App\\Http\\Controllers\\UserController@register');
+    Route::post('login', 'App\\Http\\Controllers\\UserController@authenticate');
     Route::get('businesses', 'App\\Http\\Controllers\\BusinessController@index');
-    Route::get('businesses/{business}', 'App\\Http\\Controllers\\BusinessController@show');
-    Route::post('businesses', 'App\\Http\\Controllers\\BusinessController@store');
-    Route::put('businesses/{business}', 'App\\Http\\Controllers\\BusinessController@update');
-    Route::delete('businesses/{business}', 'App\\Http\\Controllers\\BusinessController@delete');
+    Route::get('books', 'App\\Http\\Controllers\\BookController@index');
+ 
+    Route::group(['middleware' => ['jwt.verify']], function() {
+
+        Route::get('user', 'App\\Http\\Controllers\\UserController@getAuthenticatedUser');
+        
+        //CHAT
+        Route::get('chat', 'App\\Http\\Controllers\\ChatController@index');
+        Route::get('chat/{chat}', 'App\\Http\\Controllers\\ChatController@show');
+        Route::post('chat', 'App\\Http\\Controllers\\ChatController@store');
+        Route::put('chat/{chat}', 'App\\Http\\Controllers\\ChatController@update');
+        Route::delete('chat/{chat}', 'App\\Http\\Controllers\\ChatController@delete');
+        
+        //BUSINESSES
+        Route::get('businesses/{business}', 'App\\Http\\Controllers\\BusinessController@show');
+        Route::post('businesses', 'App\\Http\\Controllers\\BusinessController@store');
+        Route::put('businesses/{business}', 'App\\Http\\Controllers\\BusinessController@update');
+        Route::delete('businesses/{business}', 'App\\Http\\Controllers\\BusinessController@delete');
+        
+        //BOOKS
+        Route::get('books/{book}', 'App\\Http\\Controllers\\BookController@show');
+        Route::post('books', 'App\\Http\\Controllers\\BookController@store');
+        Route::put('books/{book}', 'App\\Http\\Controllers\\BookController@update');
+        Route::delete('books/{book}', 'App\\Http\\Controllers\\BookController@delete');
+    });    
